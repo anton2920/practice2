fe566d4e27a75089e3d2fbfd27dce6bf57ab2d73
anton <anton2920@gmail.com>
2019-03-02 18:07:09 +0300

    Anton: input

--- 10/10.c
+++ 10/10.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+void read_n(int *);
+
 main() {
 
-    /* Final output */
-    printf("Test\n");
+    /* Initializing variables */
+    int n;
+
+    /* I/O flow && VarCheck */
+    read_n(&n);
+
+    /* Main part */
+
 }
+
+void read_n(int *n) {
+
+    /* I/O flow && VarCheck */
+    while (1) {
+        printf("Type natural number «n»: ");
+        scanf("%d", n);
+        if (*n > 0) {
+            break;
+        } else {
+            printf("Error! «n» must be natural!\n\n");
+            continue;
+        }
+    }
+}
+
